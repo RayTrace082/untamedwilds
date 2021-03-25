1b336f9a1ad6a6b6c03d49efc4f2a74c30dba43f
RayTrace082 <47252286+RayTrace082@users.noreply.github.com>
2021-03-25 01:23:51 +0100

    Minor config fix

--- src/main/java/untamedwilds/config/ConfigFeatureControl.java
+++ src/main/java/untamedwilds/config/ConfigFeatureControl.java
@@ -9,11 +9,12 @@ public class ConfigFeatureControl {
     public static ForgeConfigSpec.BooleanValue addTreeOrchids;
 
     ConfigFeatureControl(final ForgeConfigSpec.Builder builder) {
-        builder.push("feature_control");
+        //builder.push("feature_control");
+        builder.comment("Options pertaining to blocks and their worldgen");
         addAnemones = builder.comment("Controls whether to add Anemones and their associated items to oceans.").define("gencontrol.anemone", true);
         addReeds = builder.comment("Controls whether to add Reeds and their associated items.").define("gencontrol.reeds", true);
         loadsOfReeds = builder.comment("Enabling this option will cause a lot more Reeds to spawn, leading to a more natural, albeit less convenient distribution").define("gencontrol.extra_reeds", false);
         addTreeOrchids = builder.comment("Controls whether to add Tree Orchids and their associated items. (NYI)").define("gencontrol.tree_orchid", false);
-        builder.pop();
+        //builder.pop();
     }
 }
