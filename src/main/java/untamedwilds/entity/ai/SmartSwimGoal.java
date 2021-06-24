39f4244aa5635f4b53175b8ce396144f822bdefd
RayTrace082 <47252286+RayTrace082@users.noreply.github.com>
2021-06-24 02:00:58 +0200

    Sleeping/Sitting animals will wake up if pushed into water, closes #20

--- src/main/java/untamedwilds/entity/ai/SmartSwimGoal.java
+++ src/main/java/untamedwilds/entity/ai/SmartSwimGoal.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.tags.FluidTags;
+import untamedwilds.entity.ComplexMobTerrestrial;
 import untamedwilds.util.EntityUtils;
 
 import java.util.EnumSet;
@@ -13,6 +14,7 @@ public class SmartSwimGoal extends Goal {
     private final MobEntity entity;
     private final float speed;
 
+    // TODO: Make use of getNavigator().getCanSwim() to check whether a mob should be able to swim or not
     public SmartSwimGoal(MobEntity entityIn) {
         this(entityIn, 0.7f);
     }
@@ -20,7 +22,7 @@ public class SmartSwimGoal extends Goal {
     public SmartSwimGoal(MobEntity entityIn, float speedIn) {
         this.entity = entityIn;
         this.speed = speedIn;
-        this.setMutexFlags(EnumSet.of(Flag.JUMP, Flag.MOVE));
+        this.setMutexFlags(EnumSet.of(Flag.MOVE));
         entityIn.getNavigator().setCanSwim(true);
     }
 
@@ -31,6 +33,18 @@ public class SmartSwimGoal extends Goal {
             return this.entity.func_233571_b_(FluidTags.WATER) > eyeHeight || this.entity.isInLava();
         }
         return false;
+    }
+
+    @Override
+    public void startExecuting() {
+        if (this.entity instanceof ComplexMobTerrestrial && !((ComplexMobTerrestrial) this.entity).canMove()) {
+            ComplexMobTerrestrial entityIn = (ComplexMobTerrestrial)this.entity;
+            entityIn.setSleeping(false);
+            entityIn.setSitting(false);
+            if (entityIn.getCommandInt() == 2) {
+                entityIn.setCommandInt(0);
+            }
+        }
     }
 
     @Override
