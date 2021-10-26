f1f46f92f678f703546699e0bfda8d38dfc01bcd
RayTrace082 <47252286+RayTrace082@users.noreply.github.com>
2021-10-26 12:26:12 +0200

    Fixed crash when starting server due to handling RenderTypes in the server

--- src/main/java/untamedwilds/init/ModBlock.java
+++ src/main/java/untamedwilds/init/ModBlock.java
@@ -26,6 +26,7 @@ import untamedwilds.block.blockentity.CritterBurrowBlockEntity;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 @SuppressWarnings("unused")
@@ -35,7 +36,7 @@ public class ModBlock {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, UntamedWilds.MOD_ID);
     public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, UntamedWilds.MOD_ID);
 
-    private final static List<Pair<RegistryObject<Block>, RenderType>> RENDER_TYPE_DATA = Lists.newArrayList();
+    private final static List<Pair<RegistryObject<Block>, String>> RENDER_TYPE_DATA = Lists.newArrayList();
 
     // Carpets
     public static RegistryObject<Block> CARPET_STRAW  = createBlock("carpet_straw", () -> new CarpetBlock(Block.Properties.create(Material.CARPET, MaterialColor.YELLOW).hardnessAndResistance(0.1F).sound(SoundType.PLANT)), ItemGroup.DECORATIONS);
@@ -59,30 +60,30 @@ public class ModBlock {
     public static RegistryObject<Block> TRAP_CAGE  = createBlock("trap_cage", () -> new CageBlock(Block.Properties.create(Material.WOOD, MaterialColor.WOOD).hardnessAndResistance(3.0F).sound(SoundType.WOOD)), ItemGroup.TRANSPORTATION);
 
     // Fauna
-    public static RegistryObject<Block> ANEMONE_ROSE_BULB  = createBlock("anemone_rose_bulb", () -> new AnemoneBlock(Block.Properties.create(Material.OCEAN_PLANT, MaterialColor.RED).hardnessAndResistance(0.1F).sound(SoundType.SLIME)), RenderType.getCutout(), ItemGroup.DECORATIONS);
-    public static RegistryObject<Block> ANEMONE_SAND  = createBlock("anemone_sand", () -> new AnemoneBlock(Block.Properties.create(Material.OCEAN_PLANT, MaterialColor.PINK).hardnessAndResistance(0.1F).sound(SoundType.SLIME)), RenderType.getCutout(), ItemGroup.DECORATIONS);
-    public static RegistryObject<Block> ANEMONE_SEBAE  = createBlock("anemone_sebae", () -> new AnemoneBlock(Block.Properties.create(Material.OCEAN_PLANT, MaterialColor.WHITE_TERRACOTTA).hardnessAndResistance(0.1F).sound(SoundType.SLIME)), RenderType.getCutout(), ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> ANEMONE_ROSE_BULB  = createBlock("anemone_rose_bulb", () -> new AnemoneBlock(Block.Properties.create(Material.OCEAN_PLANT, MaterialColor.RED).hardnessAndResistance(0.1F).sound(SoundType.SLIME)), "cutout", ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> ANEMONE_SAND  = createBlock("anemone_sand", () -> new AnemoneBlock(Block.Properties.create(Material.OCEAN_PLANT, MaterialColor.PINK).hardnessAndResistance(0.1F).sound(SoundType.SLIME)), "cutout", ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> ANEMONE_SEBAE  = createBlock("anemone_sebae", () -> new AnemoneBlock(Block.Properties.create(Material.OCEAN_PLANT, MaterialColor.WHITE_TERRACOTTA).hardnessAndResistance(0.1F).sound(SoundType.SLIME)), "cutout", ItemGroup.DECORATIONS);
 
     // Flora - Reeds
-    public static RegistryObject<Block> COMMON_REED = createBlock("flora_common_reed", () -> new ReedBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(0.1F).sound(SoundType.VINE).doesNotBlockMovement()), RenderType.getCutout(), ItemGroup.DECORATIONS, 100);
+    public static RegistryObject<Block> COMMON_REED = createBlock("flora_common_reed", () -> new ReedBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(0.1F).sound(SoundType.VINE).doesNotBlockMovement()), "cutout", ItemGroup.DECORATIONS, 100);
     // Flora - Bushes
-    public static RegistryObject<Block> BUSH_TEMPERATE = createBlock("flora_bush_temperate", () -> new UndergrowthBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(1.0F).sound(SoundType.WET_GRASS).doesNotBlockMovement()), RenderType.getCutout(), ItemGroup.DECORATIONS);
-    public static RegistryObject<Block> ELEPHANT_EAR = createBlock("flora_elephant_ear", () -> new UndergrowthBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(1.0F).sound(SoundType.WET_GRASS).doesNotBlockMovement(), AbstractBlock.OffsetType.XYZ), RenderType.getCutout(), ItemGroup.DECORATIONS);
-    public static RegistryObject<Block> HEMLOCK = createBlock("flora_hemlock", () -> new UndergrowthPoisonousBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(1.0F).sound(SoundType.WET_GRASS).doesNotBlockMovement(), AbstractBlock.OffsetType.XYZ), RenderType.getCutout(), ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> BUSH_TEMPERATE = createBlock("flora_bush_temperate", () -> new UndergrowthBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(1.0F).sound(SoundType.WET_GRASS).doesNotBlockMovement()), "cutout", ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> ELEPHANT_EAR = createBlock("flora_elephant_ear", () -> new UndergrowthBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(1.0F).sound(SoundType.WET_GRASS).doesNotBlockMovement(), AbstractBlock.OffsetType.XYZ), "cutout", ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> HEMLOCK = createBlock("flora_hemlock", () -> new UndergrowthPoisonousBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(1.0F).sound(SoundType.WET_GRASS).doesNotBlockMovement(), AbstractBlock.OffsetType.XYZ), "cutout", ItemGroup.DECORATIONS);
     // Flora - Floating
-    public static RegistryObject<Block> WATER_HYACINTH = createItemlessBlock("flora_water_hyacinth", () -> new FloatingPlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(0.0F).sound(SoundType.PLANT).doesNotBlockMovement()), RenderType.getCutout());
+    public static RegistryObject<Block> WATER_HYACINTH = createItemlessBlock("flora_water_hyacinth", () -> new FloatingPlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(0.0F).sound(SoundType.PLANT).doesNotBlockMovement()), "cutout");
     // Flora - Algae
-    public static RegistryObject<Block> AMAZON_SWORD = createBlock("flora_amazon_sword", () -> new AlgaeBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(0).sound(SoundType.WET_GRASS).doesNotBlockMovement()), RenderType.getCutout(), ItemGroup.DECORATIONS);
-    public static RegistryObject<Block> EELGRASS = createBlock("flora_eelgrass", () -> new AlgaeBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(0).sound(SoundType.WET_GRASS).doesNotBlockMovement()), RenderType.getCutout(), ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> AMAZON_SWORD = createBlock("flora_amazon_sword", () -> new AlgaeBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(0).sound(SoundType.WET_GRASS).doesNotBlockMovement()), "cutout", ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> EELGRASS = createBlock("flora_eelgrass", () -> new AlgaeBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(0).sound(SoundType.WET_GRASS).doesNotBlockMovement()), "cutout", ItemGroup.DECORATIONS);
     // Flora - Flowers
-    public static RegistryObject<Block> ORCHID_MAGENTA = createBlock("flora_orchid_magenta", () -> new EpyphitePlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.MAGENTA).hardnessAndResistance(0.0F).sound(SoundType.PLANT)), RenderType.getCutout(), ItemGroup.DECORATIONS);
-    public static RegistryObject<Block> ORCHID_PURPLE = createBlock("flora_orchid_purple", () -> new EpyphitePlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.PURPLE).hardnessAndResistance(0.0F).sound(SoundType.PLANT)), RenderType.getCutout(), ItemGroup.DECORATIONS);
-    public static RegistryObject<Block> ORCHID_PINK = createBlock("flora_orchid_pink", () -> new EpyphitePlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.PINK).hardnessAndResistance(0.0F).sound(SoundType.PLANT)), RenderType.getCutout(), ItemGroup.DECORATIONS);
-    public static RegistryObject<Block> ORCHID_RED = createBlock("flora_orchid_red", () -> new EpyphitePlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.RED).hardnessAndResistance(0.0F).sound(SoundType.PLANT)), RenderType.getCutout(), ItemGroup.DECORATIONS);
-    public static RegistryObject<Block> TITAN_ARUM = createItemlessBlock("flora_titan_arum", () -> new TitanArumBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(2.0F).sound(SoundType.WET_GRASS).doesNotBlockMovement()), RenderType.getCutout());
+    public static RegistryObject<Block> ORCHID_MAGENTA = createBlock("flora_orchid_magenta", () -> new EpyphitePlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.MAGENTA).hardnessAndResistance(0.0F).sound(SoundType.PLANT)), "cutout", ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> ORCHID_PURPLE = createBlock("flora_orchid_purple", () -> new EpyphitePlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.PURPLE).hardnessAndResistance(0.0F).sound(SoundType.PLANT)), "cutout", ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> ORCHID_PINK = createBlock("flora_orchid_pink", () -> new EpyphitePlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.PINK).hardnessAndResistance(0.0F).sound(SoundType.PLANT)), "cutout", ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> ORCHID_RED = createBlock("flora_orchid_red", () -> new EpyphitePlantBlock(Block.Properties.create(Material.PLANTS, MaterialColor.RED).hardnessAndResistance(0.0F).sound(SoundType.PLANT)), "cutout", ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> TITAN_ARUM = createItemlessBlock("flora_titan_arum", () -> new TitanArumBlock(Block.Properties.create(Material.PLANTS, MaterialColor.GREEN).hardnessAndResistance(2.0F).sound(SoundType.WET_GRASS).doesNotBlockMovement()), "cutout");
 
     // Technical Blocks
-    public static RegistryObject<Block> BURROW = createBlock("block_burrow", () -> new CritterBurrowBlock(Block.Properties.create(Material.EARTH, MaterialColor.DIRT).hardnessAndResistance(1.0F).sound(SoundType.GROUND).doesNotBlockMovement()),  RenderType.getTranslucent(), ItemGroup.DECORATIONS);
+    public static RegistryObject<Block> BURROW = createBlock("block_burrow", () -> new CritterBurrowBlock(Block.Properties.create(Material.EARTH, MaterialColor.DIRT).hardnessAndResistance(1.0F).sound(SoundType.GROUND).doesNotBlockMovement()),  "translucent", ItemGroup.DECORATIONS);
 
     // Block Entities
     public static RegistryObject<TileEntityType<CageBlockEntity>> TILE_ENTITY_CAGE = TILE_ENTITIES.register("trap_cage", () -> new TileEntityType<>(CageBlockEntity::new, Sets.newHashSet(ModBlock.TRAP_CAGE.get()), null));
@@ -92,11 +93,11 @@ public class ModBlock {
         return createBlock(name, supplier, null, group, 0);
     }
 
-    public static <B extends Block> RegistryObject<Block> createBlock(String name, Supplier<? extends B> supplier, @Nullable RenderType renderType, ItemGroup group) {
+    public static <B extends Block> RegistryObject<Block> createBlock(String name, Supplier<? extends B> supplier, @Nullable String renderType, ItemGroup group) {
         return createBlock(name, supplier, renderType, group, 0);
     }
 
-    public static <B extends Block> RegistryObject<Block> createBlock(String name, Supplier<? extends B> supplier, @Nullable RenderType renderType, ItemGroup group, int burnTime) {
+    public static <B extends Block> RegistryObject<Block> createBlock(String name, Supplier<? extends B> supplier, @Nullable String renderType, ItemGroup group, int burnTime) {
         RegistryObject<Block> block = ModBlock.BLOCKS.register(name, supplier);
         if (renderType != null) {
             RENDER_TYPE_DATA.add(new Pair<>(block, renderType));
@@ -113,7 +114,7 @@ public class ModBlock {
         return createItemlessBlock(name, supplier, null);
     }
 
-    public static <B extends Block> RegistryObject<Block> createItemlessBlock(String name, Supplier<? extends B> supplier, @Nullable RenderType renderType) {
+    public static <B extends Block> RegistryObject<Block> createItemlessBlock(String name, Supplier<? extends B> supplier, @Nullable String renderType) {
         RegistryObject<Block> block = ModBlock.BLOCKS.register(name, supplier);
         if (renderType != null) {
             RENDER_TYPE_DATA.add(new Pair<>(block, renderType));
@@ -122,8 +123,13 @@ public class ModBlock {
     }
 
     public static void registerRendering() {
-        for (Pair<RegistryObject<Block>, RenderType> i : RENDER_TYPE_DATA) {
-            RenderTypeLookup.setRenderLayer(i.getFirst().get(), i.getSecond());
+        for (Pair<RegistryObject<Block>, String> i : RENDER_TYPE_DATA) {
+            RenderType j = RenderType.getCutout();
+            if (Objects.equals(i.getSecond(), "cutout"))
+                j = RenderType.getCutout();
+            else if (Objects.equals(i.getSecond(), "translucent"))
+                j = RenderType.getTranslucent();
+            RenderTypeLookup.setRenderLayer(i.getFirst().get(), j);
         }
     }
 }
