6fc872b6d17da13a2b33573054fe6cf77d560cd3
RayTrace082 <47252286+RayTrace082@users.noreply.github.com>
2022-09-03 18:07:57 +0200

    Finished up Pampas Grass items and assets

--- src/main/java/untamedwilds/block/TallGrassBlock.java
+++ src/main/java/untamedwilds/block/TallGrassBlock.java
@@ -4,7 +4,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.*;
 import net.minecraft.world.level.block.Block;
@@ -19,17 +18,15 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import untamedwilds.init.ModBlock;
-import untamedwilds.init.ModItems;
 import untamedwilds.init.ModTags.ModBlockTags;
 
 import javax.annotation.Nullable;
 import java.util.Random;
 
 public class TallGrassBlock extends Block implements BonemealableBlock, IPostGenUpdate {
-   protected static final VoxelShape SHAPE_TRUNK = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 16.0D, 11.0D);
-   protected static final VoxelShape SHAPE_STEM = Block.box(4.0D, 0.0D, 4.0D, 12.0D, 16.0D, 12.0D);
+   protected static final VoxelShape SHAPE_TRUNK = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 16.0D, 13.0D);
    protected static final VoxelShape SHAPE_FLOWERING = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 14.0D, 13.0D);
-   public static final IntegerProperty PROPERTY_AGE = BlockStateProperties.AGE_5;
+   public static final IntegerProperty PROPERTY_AGE = BlockStateProperties.AGE_3;
    public static final IntegerProperty PROPERTY_STAGE = BlockStateProperties.STAGE;
 
    public TallGrassBlock(Properties properties) {
@@ -50,11 +47,8 @@ public class TallGrassBlock extends Block implements BonemealableBlock, IPostGen
    }
 
    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-      if (state.getValue(PROPERTY_STAGE) == 1) {
-         return SHAPE_FLOWERING;
-      }
       Vec3 vector3d = state.getOffset(worldIn, pos);
-      VoxelShape shape = state.getValue(PROPERTY_AGE) == 1 && state.getValue(PROPERTY_STAGE) == 1 ? SHAPE_STEM : SHAPE_TRUNK;
+      VoxelShape shape = (state.getValue(PROPERTY_STAGE) == 1 && state.getValue(PROPERTY_AGE) == 3) ? SHAPE_FLOWERING : SHAPE_TRUNK;
       return shape.move(vector3d.x, vector3d.y, vector3d.z);
    }
 
@@ -63,7 +57,7 @@ public class TallGrassBlock extends Block implements BonemealableBlock, IPostGen
    }
 
    public VoxelShape getCollisionShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-      return state.getValue(PROPERTY_STAGE) == 1 ? Shapes.empty() : this.getShape(state, worldIn, pos, context);
+      return (state.getValue(PROPERTY_STAGE) == 1 && state.getValue(PROPERTY_AGE) == 3) ? Shapes.empty() : this.getShape(state, worldIn, pos, context);
    }
 
    @Nullable
@@ -90,7 +84,7 @@ public class TallGrassBlock extends Block implements BonemealableBlock, IPostGen
          if (worldIn.isEmptyBlock(pos.above()) && worldIn.getLightEmission(pos.above()) >= 9) {
             int i = this.getNumReedBlocksBelow(worldIn, pos) + 1;
             if (i < 4 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt(3) == 0)) {
-               this.grow(worldIn, pos, random, i);
+               this.grow(worldIn, pos, i);
                net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);
             }
          }
@@ -124,7 +118,7 @@ public class TallGrassBlock extends Block implements BonemealableBlock, IPostGen
       int k = i + j + 1;
 
       if (worldIn.getBlockState(pos).getValue(PROPERTY_AGE) == 0) {
-         worldIn.setBlockAndUpdate(pos, this.defaultBlockState().setValue(PROPERTY_AGE, 5));
+         worldIn.setBlockAndUpdate(pos, this.defaultBlockState().setValue(PROPERTY_AGE, 3));
       }
       else {
          BlockPos blockpos = pos.above(i);
@@ -132,19 +126,19 @@ public class TallGrassBlock extends Block implements BonemealableBlock, IPostGen
          if (blockstate.getValue(PROPERTY_STAGE) == 1 || !worldIn.isEmptyBlock(blockpos.above())) {
             return;
          }
-         this.grow(worldIn, blockpos, rand, k);
+         this.grow(worldIn, blockpos, k);
       }
    }
 
-   protected void grow(Level worldIn, BlockPos posIn, Random rand, int p_220258_5_) {
-      worldIn.setBlockAndUpdate(posIn.above(), this.defaultBlockState().setValue(PROPERTY_AGE, 5));
-      worldIn.setBlockAndUpdate(posIn, this.defaultBlockState().setValue(PROPERTY_AGE, 4));
+   protected void grow(Level worldIn, BlockPos posIn, int p_220258_5_) {
+      worldIn.setBlockAndUpdate(posIn.above(), this.defaultBlockState().setValue(PROPERTY_AGE, 3));
+      worldIn.setBlockAndUpdate(posIn, this.defaultBlockState().setValue(PROPERTY_AGE, 2));
       int j = 1;
       for(int i = 3; i >= 0 && worldIn.getBlockState(posIn.below(j)).getBlock() == ModBlock.PAMPAS_GRASS.get(); --i) {
-         worldIn.setBlockAndUpdate(posIn.below(j), ModBlock.PAMPAS_GRASS.get().defaultBlockState().setValue(PROPERTY_AGE, Math.max(i, 1)));
+         worldIn.setBlockAndUpdate(posIn.below(j), ModBlock.PAMPAS_GRASS.get().defaultBlockState().setValue(PROPERTY_AGE, 1));
          j++;
       }
-      worldIn.setBlockAndUpdate(posIn.above(), this.defaultBlockState().setValue(PROPERTY_AGE, 5));
+      worldIn.setBlockAndUpdate(posIn.above(), this.defaultBlockState().setValue(PROPERTY_AGE, 3));
       if (p_220258_5_ == 2) {
          for(int i = 0; i < 3 && worldIn.getBlockState(posIn.above(i)).getBlock() == ModBlock.PAMPAS_GRASS.get(); ++i) {
             worldIn.setBlock(posIn.above(i), this.defaultBlockState().setValue(PROPERTY_AGE, worldIn.getBlockState(posIn.above(i)).getValue(PROPERTY_AGE)).setValue(PROPERTY_STAGE, 1), 3);
@@ -170,18 +164,10 @@ public class TallGrassBlock extends Block implements BonemealableBlock, IPostGen
       return i;
    }
 
-   protected ItemLike getSeedsItem() {
-      return ModItems.SEED_ZIMBABWE_ALOE.get();
-   }
-
-   public ItemStack getCloneItemStack(BlockGetter p_60261_, BlockPos p_60262_, BlockState p_60263_) {
-      return new ItemStack(this.getSeedsItem());
-   }
-
    public void updatePostGen(LevelAccessor worldIn, BlockPos pos) {
       int i;
       for (i = 0; i < 3 && (worldIn.getBlockState(pos.above(i)).isAir() || worldIn.getBlockState(pos.above(i)).getBlock() == ModBlock.PAMPAS_GRASS.get()); i++) {
-         worldIn.setBlock(pos.above(i), this.defaultBlockState().setValue(PROPERTY_AGE, Math.max(1, i + 3)), 3);
+         worldIn.setBlock(pos.above(i), this.defaultBlockState().setValue(PROPERTY_AGE, Math.max(1, i + 1)), 3);
       }
    }
 }
